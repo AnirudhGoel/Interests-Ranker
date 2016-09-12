00c6cc93c50adcb00f0d236ab160c0d772ee14de
Anirudh Goel <Anirudhgoel.delhi@gmail.com>
2016-09-12 11:56:54 +0530

    corrected API address

--- sample/index.js
+++ sample/index.js
@@ -8,7 +8,7 @@ function addUser(event) {
 
 		usernames = usernames.replace(/,\s*$/, "");
 
-		var url = "https://interests-ranker.herokuapp.com/topics.php?user=" + usernames;
+		var url = "/topics.php?user=" + usernames;
 		$("#output").html("");
 		console.log(url);
 		$.get(url, function(data){
